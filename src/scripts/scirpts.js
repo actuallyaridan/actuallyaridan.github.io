a7fce71db119843fc8466cc2589c180592cf09a3
aridan <adnanbukvic862@gmail.com>
2024-05-24 13:54:06 +0200

    undid

--- src/scripts/scirpts.js
+++ src/scripts/scirpts.js
@@ -1,7 +1,10 @@
 window.onload = function () {
+    // Parses the document body and    
+    // inserts <img> tags in place of Unicode Emojis    
     twemoji.parse(document.body,
         { folder: 'svg', ext: '.svg' } // This is to specify to Twemoji to use SVGs and not PNGs
     );
+
 }
 
 window.addEventListener('keydown', function (event) {
